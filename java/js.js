e9d31df1d359745a47d9fa5dab19d65dcfb0c1c6
platanob <bcarrasco2022@alu.uct.cl>
2022-10-03 15:52:25 -0300

    ajustes

--- java/js.js
+++ java/js.js
@@ -2,9 +2,11 @@
 function fun() {
     var nombre = document.getElementById("nombre").value;
     var email = document.getElementById("email").value;
+    var producto = document.getElementById("producto").value;
     msj = document.getElementById("mensaje").value;
     alert("Tu nombre es: " + nombre + "\n" +
         "Tu email es:" + email + "\n" +
+        "Tu producto es:" + producto + "\n" +
         "Tu mensaje: " + msj);
 }
 
