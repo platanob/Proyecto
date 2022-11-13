c2e303a710984dfdf7a9ef5929668475c4eaf614
der-kaffe <jcardenas2022@alu.uct.cl>
2022-11-13 04:23:19 -0300

    cosas

--- html/edit.php
+++ html/edit.php
@@ -3,18 +3,18 @@
 require 'cfg.php';
 
 $conn = new mysqli($servername, $username, $password, $dbname);
-
+session_start();
+$usuario = $_SESSION['usuario'];
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 }
 $ID = $_POST['ID'];
-$nombre = $_POST['nombre'];
 $correo = $_POST['email'];
 $mensaje = $_POST['mensaje'];
 $telefono = $_POST['telefono'];
 $idpro = $_POST['producto'];
 
-$sql = "UPDATE `pedidos` SET `nombre` = '$nombre',`correo` = '$correo', `comentario` ='$mensaje', `telefono` = '$telefono', `id_producto` = '$idpro' WHERE `pedidos`.`id_pedido` = $ID";
+$sql = "UPDATE `pedidos` SET `pedidos`.`id_producto`= '$idpro',`correo` = '$correo', `comentario` = '$mensaje', `telefono` = '$telefono' WHERE `pedidos`.`id_pedido` = $ID and `pedidos`.`nombre` = '$usuario'";
 
 $result = $conn->query($sql);
 
